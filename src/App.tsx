43e25ef3bcf9855a383e113ef3ede73034281125
이희근 <dasepa@10.nate.com>
2026-02-16 14:47:09 +0900

    Fix: Switch to HashRouter for GitHub Pages support

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MobileLayout from './components/layout/MobileLayout';
 import Home from './pages/Home';
 import Schedule from './pages/Schedule';
@@ -8,7 +8,7 @@ import Records from './pages/Records';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MobileLayout />}>
           <Route index element={<Home />} />
@@ -18,7 +18,7 @@ function App() {
           <Route path="records" element={<Records />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
